357d1a1391bef2b7f54d65159c210ca13e671e4a
Alexander Farizanov <farizanov@gmail.com>
2023-07-11 13:57:59 +0300

    no message

--- FakeNFT/Screens/Stats/StatPage/StatPageModel.swift
+++ FakeNFT/Screens/Stats/StatPage/StatPageModel.swift
@@ -4,6 +4,9 @@ final class StatPageModel {
     let defaultNetworkClient = DefaultNetworkClient()
 
     func getUsers(completion: @escaping (Result<[User], Error>) -> Void) {
+        print("users list")
+        print(Config.baseUrl)
+        print("/users")
         let request = Request(endpoint: URL(string: Config.baseUrl + "/users"), httpMethod: .get)
         print("users list")
         defaultNetworkClient.send(request: request, type: [User].self, onResponse: completion)
